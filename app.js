9e151a28f3bdeb573aa20e78d5c3146b4e1ecc6b
Batoru1 <103144015+Batoru1@users.noreply.github.com>
2023-09-23 12:17:17 +0300

    just starting

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-const miniSquare = document.querySelector('.mini-square');
+const container = document.querySelector('.container');
+
+for (let i = 1; i <= 16 * 16; i++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+  square.textContent = i;
+  container.appendChild(square);
+}
