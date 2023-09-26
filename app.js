c745b3e151e40ebbb08b7692c0f65a4ce01aed74
Batoru1 <103144015+Batoru1@users.noreply.github.com>
2023-09-26 20:12:34 +0300

    fun rainbow sketch

--- app.js
+++ app.js
@@ -9,3 +9,22 @@ function createGrid(rows, columns) {
 }
 
 createGrid(16, 16);
+
+const squares = document.querySelectorAll('.square');
+
+squares.forEach(function (square) {
+  square.addEventListener('mouseenter', function () {
+    // Change the background color to a random color
+    square.style.backgroundColor = getRandomColor();
+  });
+});
+
+// Function to generate a random color in hex format (#RRGGBB)
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
