1bb8239a0fbfc6e1698a1b6c9d40c3a4d74a3773
Batoru1 <103144015+Batoru1@users.noreply.github.com>
2023-10-17 06:49:17 +0300

    changing grid size and layout

--- app.js
+++ app.js
@@ -3,22 +3,31 @@ const sliderInput = document.getElementById('slider-input');
 const sliderValue = document.getElementById('slider-value');
 
 function createGrid(rows, columns) {
-  // Clear the container by removing all child elements
+  // Remove existing square elements
   while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
+  // Calculate the square size based on the slider value and container width
+  const containerWidth = container.clientWidth;
+  const squareSize = containerWidth / columns;
 
+  // Create and append the grid squares with adjusted size
   for (let i = 0; i < rows * columns; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.style.width = squareSize + 'px';
+    square.style.height = squareSize + 'px';
     container.appendChild(square);
   }
 }
 
 // Event listener to update the slider value display and create the grid
-sliderInput.addEventListener('input');
+sliderInput.addEventListener('input', function () {
+  sliderValue.textContent = sliderInput.value;
+  createGrid(sliderInput.value, sliderInput.value); // Create the grid based on the slider value
+});
 
-createGrid(16, 16);
+createGrid(1, 1);
 
 let squares = document.querySelectorAll('.square');
 
